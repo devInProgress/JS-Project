4ec9322ea184f0ad7ddba5002f0c946d79c8e7cb
Naved <naved1234@gmail.com>
2018-08-23 15:09:35 +0530

    some code changes

---
+++ app.js
@@ -0,0 +1,22 @@
+"use strict";
+
+searchButton.addEventListener('click', searchWeather);
+
+function searchWeather() {
+  var cityName = searchCity.value;
+  if (cityName.trim().length == 0) {
+    return alert('Please enter a City Name');
+  }
+  var http = new XMLHttpRequest();
+  var apiKey = '0773d19c21418bec6a46b58f59a38d89';
+  var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName;
+  var method = 'GET';
+  http.open(method, url);
+  http.onreadystatechange = function () {
+    if (http.readyState == XMLHttpRequest.DONE && http.status === 200) {
+      var data = JSON.parse(http.responseText);
+    } else if (http.readyState == XMLHttpRequest.DONE) {
+      alert('Something went wrong!');
+    }
+  }
+}
---
+++ weather-data.js
@@ -0,0 +1,8 @@
+"use strict";
+
+function Weather(cityName, description) {
+  this.cityName = cityName;
+  this.description = description;
+  this._temperature = '';
+
+}
